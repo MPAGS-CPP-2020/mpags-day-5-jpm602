ca5467b5aeb9b99397a2e1060c537feda118ba24
Jack Matthews <jpm602@student.bham.ac.uk>
2020-11-27 14:13:54 +0000

    pABC for a cipher interface

---
+++ src/MPAGSCipher/Cipher.hpp
@@ -0,0 +1,39 @@
+#ifndef CIPHER_HPP
+#define CIPHER_HPP
+
+#include <string>
+
+#include "CipherMode.hpp"
+
+/**
+ * \file Cipher.hpp
+ * \brief Contains the declaration of the Cipher purely abstract base class
+ */
+
+/**
+ * \class Cipher
+ * \brief Purely abstract base class for a cipher
+ */
+
+class Cipher
+{
+    public:
+        Cipher() = default;
+        Cipher(const Cipher& rhs) = default;
+        Cipher(Cipher&& rhs) = default;
+        Cipher& operator=(const Cipher& rhs) = default;
+        Cipher& operator=(Cipher&& rhs) = default;
+        virtual ~Cipher() = default;
+
+        /**
+        * Apply the cipher to the provided text
+        *
+        * \param inputText the text to encrypt or decrypt
+        * \param cipherMode whether to encrypt or decrypt the input text
+        * \return the result of applying the cipher to the input text
+        */
+        virtual std::string applyCipher( const std::string& input,
+                                            const CipherMode mode ) const = 0;
+};
+
+#endif
